8d36d9ec83ee0cae25fff8a1893052ccf6aefc80
SIU\extcprudhomme <cyril.prudhomme.ext@cibtp.fr>
2023-03-28 11:32:20 +0200

    Init Project

--- docker-telescope/src/app/volumes/volumes.page.ts
+++ docker-telescope/src/app/volumes/volumes.page.ts
@@ -18,7 +18,7 @@ export class VolumesPage implements OnInit {
   ngOnInit(): void {
   }
 
-  setOpenContainerDetails(isOpen: boolean) {
+  setOpenDetails(isOpen: boolean) {
     this.isModalDetailsOpen = isOpen;
   }
 
